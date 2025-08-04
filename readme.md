29b6546625f89afd578a707275289fd9d2523076
andrew <andrew>
2025-08-04 13:57:15 +1000

    Updating script

--- readme.md
+++ readme.md
@@ -1,7 +1,12 @@
 # Todo 
 
- - Deploy to PC
- - Mullvad: set up auto configuration
- - Make a better unlock mechanism
- - Update to USB3 YubiKey
+- Make a better unlock mechanism
+   * A short cut that opens st in su
+   * runs script automatically
+- Deploy to PC
+- Update to USB3 YubiKey
+
+# Windows Installation
+
+- Secure boot should be set to No
 
--- scripts/nixos_installation.sh
+++ scripts/nixos_installation.sh
@@ -3,29 +3,36 @@
 # parted /dev/ -- mkpart primary 1MB -8GB
 # parted /dev/ -- mkpart primary linux-swap -8GB 100%
 
+DISK="/dev/"
+PART1="/dev/"
+PART2="/dev/"
+PART3="/dev/"
+PART4="/dev/"
+PART5="/dev/"
+
 # Partition (UEFI)
-parted /dev/ -- mklabel gpt
-parted /dev/ -- mkpart ESP fat32 1MB 512MB
-parted /dev/ -- mkpart nix ext4 512MB 100GB
-parted /dev/ -- mkpart persist ext4 100GB 150GB
-parted /dev/ -- mkpart persist-enc ext4 150GB 220GB
-parted /dev/ -- mkpart swap linux-swap 220GB 100%
-parted /dev/ -- set 5 esp on
+parted $DISK -- mklabel gpt
+parted $DISK -- mkpart ESP fat32 1MB 512MB
+parted $DISK -- mkpart nix ext4 512MB 100GB
+parted $DISK -- mkpart persist ext4 100GB 150GB
+parted $DISK -- mkpart persist-enc ext4 150GB 220GB
+parted $DISK -- mkpart swap linux-swap 220GB 100%
+parted $DISK -- set 5 esp on
 
 # Installation
-cryptsetup luksFormat /dev/
+cryptsetup luksFormat $PART4
 cryptsetup open /dev/ persist-enc
 mkdir -p /mnt/boot /mnt/nix /mnt/persist /mnt/persist-enc
 
-mkfs.fat -F 32 -n boot /dev/        # (for UEFI systems only)
+mkfs.fat -F 32 -n boot $PART1        # (for UEFI systems only)
 # mkfs.ext4 -L boot /dev/           # (for MBR systems only)
-mkfs.ext4 -L nix /dev/
-mkfs.ext4 -L persist /dev/
+mkfs.ext4 -L nix $PART2
+mkfs.ext4 -L persist $PART3
 mkfs.ext4 -L persist-enc /dev/mapper/persist-env
-mkswap -L swap /dev/
-swapon /dev/
+mkswap -L swap $PART5
+swapon $PART5
 
-mount -t tmpfs -o size=1G /mnt
+mount -t tmpfs -o size=1G tmpfs /mnt
 mount -o umask=077 /dev/disk/by-label/boot /mnt/boot # (for UEFI systems only)
 # mount /dev/disk/by-label/boot /mnt/boot # (for MBR systems only)
 mount /dev/disk/by-label/nix /mnt/nix
@@ -33,6 +40,8 @@ mount /dev/disk/by-label/persist /mnt/persist
 mount /dev/disk/by-label/persist-enc /mnt/persist-enc
 
 nixos-generate-config --root /mnt
+exit
+
 vim /mnt/etc/nixos/configuration.nix
 # environment.systemPackages = [ pkgs.vim ]
 # nix.settings.experimental-features = [ "nix-command" "flakes" ];
