6eaaa2fe620565b306953b9563251c714a361787
andrew <andrew>
2025-08-04 14:08:23 +1000

    Updating script

--- scripts/nixos_installation.sh
+++ scripts/nixos_installation.sh
@@ -3,6 +3,7 @@
 # parted /dev/ -- mkpart primary 1MB -8GB
 # parted /dev/ -- mkpart primary linux-swap -8GB 100%
 
+echo '--- Setting Variables ---'
 DISK="/dev/"
 PART1="/dev/"
 PART2="/dev/"
@@ -10,7 +11,7 @@ PART3="/dev/"
 PART4="/dev/"
 PART5="/dev/"
 
-# Partition (UEFI)
+echo '--- Partitioning (UEFI) ---'
 parted $DISK -- mklabel gpt
 parted $DISK -- mkpart ESP fat32 1MB 512MB
 parted $DISK -- mkpart nix ext4 512MB 100GB
@@ -19,11 +20,11 @@ parted $DISK -- mkpart persist-enc ext4 150GB 220GB
 parted $DISK -- mkpart swap linux-swap 220GB 100%
 parted $DISK -- set 5 esp on
 
-# Installation
+echo '--- Disk Encryption ---'
 cryptsetup luksFormat $PART4
 cryptsetup open /dev/ persist-enc
-mkdir -p /mnt/boot /mnt/nix /mnt/persist /mnt/persist-enc
 
+echo '--- Formatting Disks ---'
 mkfs.fat -F 32 -n boot $PART1        # (for UEFI systems only)
 # mkfs.ext4 -L boot /dev/           # (for MBR systems only)
 mkfs.ext4 -L nix $PART2
@@ -32,13 +33,16 @@ mkfs.ext4 -L persist-enc /dev/mapper/persist-env
 mkswap -L swap $PART5
 swapon $PART5
 
+echo '--- Mounting Disks ---'
 mount -t tmpfs -o size=1G tmpfs /mnt
+mkdir -p /mnt/boot /mnt/nix /mnt/persist /mnt/persist-enc
 mount -o umask=077 /dev/disk/by-label/boot /mnt/boot # (for UEFI systems only)
 # mount /dev/disk/by-label/boot /mnt/boot # (for MBR systems only)
 mount /dev/disk/by-label/nix /mnt/nix
 mount /dev/disk/by-label/persist /mnt/persist
 mount /dev/disk/by-label/persist-enc /mnt/persist-enc
 
+echo '--- Generating Config ---'
 nixos-generate-config --root /mnt
 exit
 
