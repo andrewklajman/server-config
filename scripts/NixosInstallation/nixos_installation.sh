e3e0dfc513e062713616a0c78dbc4a12b2b1e083
andrew <andrew>
2025-08-11 09:25:38 +1000

    Cleaning up scripts directory

---
+++ scripts/NixosInstallation/nixos_installation.sh
@@ -0,0 +1,66 @@
+# # Partition (MBR)
+# parted /dev/ -- mklabel msdos
+# parted /dev/ -- mkpart primary 1MB -8GB
+# parted /dev/ -- mkpart primary linux-swap -8GB 100%
+
+echo '--- Setting Variables ---'
+LUKS_PASS=""
+DISK="/dev/nvme0n1"
+PART1="/dev/nvme0n1p1"
+PART2="/dev/nvme0n1p2"
+PART3="/dev/nvme0n1p3"
+PART4="/dev/nvme0n1p4"
+PART5="/dev/nvme0n1p5"
+
+echo '--- Partitioning (UEFI) ---'
+parted $DISK -- mklabel gpt
+parted $DISK -- mkpart ESP fat32 1MB 512MB
+parted $DISK -- mkpart nix ext4 512MB 100GB
+parted $DISK -- mkpart persist ext4 100GB 150GB
+parted $DISK -- mkpart persist-enc ext4 150GB 200GB
+parted $DISK -- mkpart swap linux-swap 200GB 210GB
+parted $DISK -- set 1 esp on
+
+echo '--- Disk Encryption ---'
+echo $LUKS_PASS | cryptsetup -q luksFormat $PART4
+echo $LUKS_PASS | cryptsetup -q open $PART4 persist-enc
+
+echo '--- Formatting Disks ---'
+mkfs.fat -F 32 -n boot $PART1        # (for UEFI systems only)
+# mkfs.ext4 -L boot /dev/           # (for MBR systems only)
+mkfs.ext4 -q -L nix $PART2
+mkfs.ext4 -q -L persist $PART3
+mkfs.ext4 -q -L persist-enc /dev/mapper/persist-enc
+mkswap -q -L swap $PART5
+swapon $PART5
+
+echo '--- Mounting Disks ---'
+mount -t tmpfs -o size=1G tmpfs /mnt
+mkdir -p /mnt/boot /mnt/nix /mnt/persist /mnt/persist-enc
+mount -o umask=077 /dev/disk/by-label/boot /mnt/boot # (for UEFI systems only)
+# mount /dev/disk/by-label/boot /mnt/boot # (for MBR systems only)
+mount /dev/disk/by-label/nix /mnt/nix
+mount /dev/disk/by-label/persist /mnt/persist
+mount /dev/disk/by-label/persist-enc /mnt/persist-enc
+
+echo '--- Generating and Adjust Config ---'
+nixos-generate-config --root /mnt
+sed -i '16 i environment.systemPackages = with pkgs; [ vim git ];' /mnt/etc/nixos/configuration.nix
+sed -i '16 i nix.settings.experimental-features = [ "nix-command" "flakes" ];' /mnt/etc/nixos/configuration.nix
+sed -i '16 i users.users.root.initialPassword = "pass";' /mnt/etc/nixos/configuration.nix
+
+echo '--- Installation ---'
+nixos-install
+
+echo '--- File Backup ---'
+mkdir /mnt/persist/original_config
+cp /mnt/etc/nixos/* /mnt/persist/original_config
+cd /mnt/persist
+git clone https://www.github.com/andrewklajman/server-config
+
+echo '--- Restart ---'
+restart
+
+
+
+
