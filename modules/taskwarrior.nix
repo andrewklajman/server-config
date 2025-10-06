e8baffbe6eead66adc7f0014db29f2c8ca9cdf0f
andrew <andrew>
2025-10-06 16:51:53 +1100

    Implemented task warriror

--- modules/taskwarrior.nix
+++ modules/taskwarrior.nix
@@ -1,14 +1,29 @@
-{ config, pkgs, lib, localLuks, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   cfg = config.taskwarrior;
+  luks = config.consts.localLuks.mountPoint;
 in
 {
   options.taskwarrior = {
     enable = lib.mkEnableOption "taskwarrior";
+    taskrc = lib.mkOption {
+      type = lib.types.str;
+      default = "${luks}/Documents/taskwarrior/taskrc";
+    };
+    taskdata = lib.mkOption {
+      type = lib.types.str;
+      default = "${luks}/Documents/taskwarrior/taskdata";
+    };
   };
   
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ pkgs.taskwarrior3 ];
+    environment = {
+      systemPackages = [ pkgs.taskwarrior3 ];
+      sessionVariables = { 
+        TASKRC = "${cfg.taskrc}";
+        TASKDATA = "${cfg.taskdata}";
+      };
+    };
   };
 }
