717edde09d0cc98749eedcc6c2089820aed0c8aa
andrew <andrew.klajman@gmail.com>
2025-11-20 18:20:04 +1100

    server update

--- hosts/dell-server/additional-config.nix
+++ hosts/dell-server/additional-config.nix
@@ -5,6 +5,11 @@
   config = {
     nix.settings.experimental-features   = [ "nix-command" "flakes" ];
 
+    networking.firewall = {
+      allowedTCPPorts = [ 80 443 ];
+      allowedUDPPorts = [ 53 ];
+    };
+
     basePackages.enable       = true;
     bootlimit.enable          = true;
     diskusage.enable          = true;
@@ -28,6 +33,44 @@
           email = [ "andrew.klajman@gmail.com" ];
         };
       };
+    };
+
+
+    tailscale-userspace = {
+      enable = true;
+      configDir = "/root/tailscale";
+    };
+
+    services.dnsmasq = {
+      enable = true;
+      settings = {
+        interface = "tailscale0";
+        address = [
+          "/abs/100.70.63.101"
+          "/torrent/100.70.63.101"
+        ];
+        server = [ "8.8.8.8" "8.8.4.4" ];
+      };
+    
+    };
+
+    services.nginx = {
+      enable = true;
+      config = ''${builtins.readFile ../../config/dell-server-nginx.conf}'';
+    };
+
+    services.audiobookshelf = {
+      enable = true;
+      host = "0.0.0.0";
+    };
+    services.qbittorrent = {
+      enable = true;
+      openFirewall = true;
+#/root/torrent/config/qBittorrent.conf
+    };
+    mullvad = {
+      enable = true;
+      configDir = "/root/mullvad/config";
     };
 
   };
